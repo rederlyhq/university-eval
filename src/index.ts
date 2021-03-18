5279ec1210ef3575bd2442285d3a936830c9bc83
red-tt-bear <red.tt.bear@gmail.com>
2021-03-18 18:46:31 -0400

    Added more useful output and some more error handling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env -S npx ts-node
+
 require('dotenv').config();
 import DatabaseController from "./database-controller";
 import { queries } from "./queries";
@@ -5,19 +7,25 @@ import { queries } from "./queries";
 (async () => {
     const databaseController = new DatabaseController();
     await databaseController.awaitConnection();
+    console.log(`/* RUN ON ${new Date()} */\n\n\n`);
 
-    for (let i = 0; i < queries.length; i++) {
-        const query = queries[i];
-        const result = await databaseController.query(query.query);
-        console.log(query.name);
-        console.log(query.description);
-        // console.log(query.query);
-        console.log(JSON.stringify(result.rows, null, 2));
-        console.log('\n\n\n');
-    }
     try {
-        await databaseController?.close();
+        for (let i = 0; i < queries.length; i++) {
+            const query = queries[i];
+            const result = await databaseController.query(query.query);
+            console.log(`/* ${query.name} */`);
+            console.log(`/* ${query.description} */`);
+            // console.log(query.query);
+            console.log(JSON.stringify(result.rows, null, 2));
+            console.log('\n\n\n');
+        }            
     } catch (e) {
-        console.error('Unexpected error closing db connection', e);
+        console.error('Unexpected error running queries', e);
+    } finally {
+        try {
+            await databaseController?.close();
+        } catch (e) {
+            console.error('Unexpected error closing db connection', e);
+        }
     }
 })();
